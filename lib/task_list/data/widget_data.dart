952d5412e2dbade255881592ce613d114ba47cd8
Donald Tam (ESD - Mobile Application Developer, Digital Solutions) <donaldtam@esdlife.com>
2023-07-28 16:04:38 +0800

    added files

---
+++ lib/task_list/data/widget_data.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+const taskListPadding = 20.0;
+const taskListItemSpacing = 12.0;
+
+const startTaskListItemEdgeInsets = EdgeInsets.only(
+  left: taskListPadding,
+  right: taskListPadding,
+);
+
+const middleTaskListItemEdgeInsets = EdgeInsets.only(
+  top: taskListItemSpacing,
+  left: taskListPadding,
+  right: taskListPadding,
+);
+
+const endTaskListItemEdgeInsets = EdgeInsets.only(
+  top: taskListItemSpacing,  
+  left: taskListPadding,
+  right: taskListPadding,
+);
---
+++ lib/task_list/functions/widget_functions.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/widgets.dart';
+import 'package:too_many_tasks/common/models/task.dart';
+import 'package:too_many_tasks/common/widgets/proportion_box/proportion_box.dart';
+import 'package:too_many_tasks/task_list/data/widget_data.dart';
+import 'package:too_many_tasks/task_list/widgets/task_card/state.dart' as task_card;
+import 'package:too_many_tasks/task_list/widgets/task_card/task_card.dart' as task_card;
+
+enum _TaskType {
+  pinned,
+  unpinned,
+}
+
+List<Widget Function()> pinnedTasksBuilders(
+  List<Task> tasks,
+  List<task_card.State> cardStates,
+  task_card.Listener listener,
+) => _taskBuilders(
+  _TaskType.pinned,
+  tasks,
+  cardStates,
+  listener
+);
+
+List<Widget Function()> unpinnedTasksBuilders(
+  List<Task> tasks,
+  List<task_card.State> cardStates,
+  task_card.Listener listener,
+) => _taskBuilders(
+  _TaskType.unpinned,
+  tasks,
+  cardStates,
+  listener
+);
+
+List<Widget Function()> _taskBuilders(
+  _TaskType type,
+  List<Task> tasks,
+  List<task_card.State> cardStates,
+  task_card.Listener listener,
+) {
+  final builders = List<Widget Function()>.empty(growable: true);
+
+  for (var index = 0; index < cardStates.length; index++) {
+    final task = tasks[index];
+    final shouldInclude = 
+      task.pinned && type == _TaskType.pinned ||
+      !task.pinned && type == _TaskType.unpinned;
+    if (!shouldInclude) continue;
+    final cardState = cardStates[index]; 
+    final visibility = switch (cardState) {
+      task_card.BeingAdded(animationValue: final animationValue) =>
+        animationValue,
+      task_card.Ready() => 1.0,
+      task_card.BeingRemoved(animationValue: final animationValue) =>
+        1 - animationValue,
+      task_card.Removed() => null,
+    };
+    if (visibility == null) continue;
+    builders.add(
+      () => ProportionSize(
+        proportion: visibility,
+        child: Opacity(
+          opacity: visibility,
+          child: Padding(
+            padding: taskItemPadding(tasks.length, index),
+            child: task_card.TaskCard(index, task, listener),
+          ),
+        )
+      ),
+    );
+  }
+
+  return builders;
+}
+
+EdgeInsets taskItemPadding(int length, int index) {
+  if (index == 0) {
+    return startTaskListItemEdgeInsets;
+  } else if (index == length - 1) {
+    return middleTaskListItemEdgeInsets;
+  } else {
+    return endTaskListItemEdgeInsets;
+  }
+}
